271ce24f65ff9f1af0daed861f9949f1f9228543
SpaceDuckie <151963828+SpaceDuckie@users.noreply.github.com>
2023-11-25 15:32:25 +0000

    player movement controller

---
+++ Assets/testPlayerScript.cs
@@ -0,0 +1,131 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class testPlayerScript : MonoBehaviour
+{
+    
+    private Rigidbody2D rigidBody;
+    private SpriteRenderer sprite;
+    
+    //[SerializeField]
+
+    private float moveConst = 200;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+        sprite = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rigidBody.velocity = Vector3.zero;
+        if (Input.GetKey("w"))
+        {
+            rigidBody.AddForce(transform.up* moveConst * Time.deltaTime);
+            Debug.Log("w");
+            sprite.flipY = false;
+
+            if (Input.GetKey("a"))
+            {
+                rigidBody.AddForce(transform.right* -moveConst * Time.deltaTime);
+                Debug.Log("a");
+                sprite.flipX = true;
+            }
+            else if (Input.GetKey("d"))
+            {
+                rigidBody.AddForce(transform.right* moveConst * Time.deltaTime);
+                Debug.Log("d");
+                sprite.flipX = false;
+            }
+            else if (Input.GetKey("s"))
+            {
+                rigidBody.AddForce(transform.up* -moveConst * Time.deltaTime);
+                Debug.Log("s");
+                sprite.flipY = true;
+            }
+
+        }
+
+        else if (Input.GetKey("a"))
+        {
+            rigidBody.AddForce(transform.right* -moveConst * Time.deltaTime);
+            Debug.Log("a");
+            sprite.flipX = true;
+
+            if (Input.GetKey("w"))
+            {
+                rigidBody.AddForce(transform.up* moveConst * Time.deltaTime);
+                Debug.Log("w");
+                sprite.flipY = false;
+            }
+            else if (Input.GetKey("d"))
+            {
+                rigidBody.AddForce(transform.right* moveConst * Time.deltaTime);
+                Debug.Log("d");
+                sprite.flipX = false;
+            }
+            else if (Input.GetKey("s"))
+            {
+                rigidBody.AddForce(transform.up* -moveConst * Time.deltaTime);
+                Debug.Log("s");
+                sprite.flipY = true;
+            }
+
+        }
+        else if (Input.GetKey("d"))
+        {
+            rigidBody.AddForce(transform.right* moveConst * Time.deltaTime);
+            Debug.Log("d");
+            sprite.flipX = false;
+
+            if (Input.GetKey("w"))
+            {
+                rigidBody.AddForce(transform.up* moveConst * Time.deltaTime);
+                Debug.Log("w");
+                sprite.flipY = false;
+            }
+            else if (Input.GetKey("d"))
+            {
+                rigidBody.AddForce(transform.right* moveConst * Time.deltaTime);
+                Debug.Log("d");
+                sprite.flipX = false;
+            }
+            else if (Input.GetKey("s"))
+            {
+                rigidBody.AddForce(transform.up* -moveConst * Time.deltaTime);
+                Debug.Log("s");
+                sprite.flipY = true;
+            }
+        }
+        else if (Input.GetKey("s"))
+        {
+            rigidBody.AddForce(transform.up* -moveConst * Time.deltaTime);
+            Debug.Log("s");
+            sprite.flipY = true;
+
+            if (Input.GetKey("w"))
+            {
+                rigidBody.AddForce(transform.up* moveConst * Time.deltaTime);
+                Debug.Log("w");
+                sprite.flipY = false;
+            }
+            else if (Input.GetKey("d"))
+            {
+                rigidBody.AddForce(transform.right* moveConst * Time.deltaTime);
+                Debug.Log("d");
+                sprite.flipX = false;
+            }
+            else if (Input.GetKey("s"))
+            {
+                rigidBody.AddForce(transform.up* -moveConst * Time.deltaTime);
+                Debug.Log("s");
+                sprite.flipY = true;
+            }
+        }
+    }
+}
