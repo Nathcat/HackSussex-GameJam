92d20140d1174519a92459bee8083a9c52a32d45
lionbanana09 <139818521+lionbanana09@users.noreply.github.com>
2023-11-25 23:55:16 +0000

    fish

---
+++ Assets/Tasks/wires.cs
@@ -0,0 +1,93 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class wires : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject redwire;
+    public GameObject bluewire;
+    public GameObject yellowwire;
+    public GameObject greenwire;
+    public int welded=0;
+    public GameObject eventsystem;
+    public GameObject host;
+    public string current = "";
+    void Start()
+    {
+        host.gameObject.SetActive(false);
+        redwire.gameObject.SetActive(false);
+        bluewire.gameObject.SetActive(false);
+        greenwire.gameObject.SetActive(false);
+        yellowwire.gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    public void startblue() {
+        current = "blue";
+    }
+    public void startgreen() { 
+        current = "green";
+    }
+    public void startred()
+    {
+        current = "red";
+    }
+    public void startyellow()
+    {
+        current = "yellow";
+    }
+    public void endblue()
+    {
+        if (current == "blue") { 
+            bluewire.gameObject.SetActive(true);
+        }
+        welded++;
+        if (welded == 4)
+        {
+            eventsystem.GetComponent<task_manager>().tasks += 1;
+            host.gameObject.SetActive(false);
+        }
+    }
+    public void endgreen()
+    {
+        if (current == "green")
+        {
+            greenwire.gameObject.SetActive(true);
+        }
+        welded++;
+        if (welded == 4)
+        {
+            eventsystem.GetComponent<task_manager>().tasks += 1;
+            host.gameObject.SetActive(false);
+        }
+    }
+    public void endred()
+    {
+        if (current == "red")
+        {
+            redwire.gameObject.SetActive(true);
+        }
+        welded++;
+        if (welded == 4)
+        {
+            eventsystem.GetComponent<task_manager>().tasks += 1;
+            host.gameObject.SetActive(false);
+        }
+    }
+    public void endyellow()
+    {
+        if (current == "yellow")
+        {
+            yellowwire.gameObject.SetActive(true);
+        }
+        welded++;
+        if (welded == 4)
+        {
+            eventsystem.GetComponent<task_manager>().tasks += 1;
+            host.gameObject.SetActive(false);
+        }
+    }
+
+}
