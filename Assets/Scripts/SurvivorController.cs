331ba0a47b687d901fda927484b46e4ad068a367
Nathcat <74734054+Nathcat@users.noreply.github.com>
2023-11-26 06:38:48 +0000

    Update SurvivorController.cs

--- Assets/Scripts/SurvivorController.cs
+++ Assets/Scripts/SurvivorController.cs
@@ -6,7 +6,7 @@ public class SurvivorController : MonoBehaviour
 {
     public void Update() {
         if (Input.GetMouseButtonDown(0)) {
-            Collider2D[] colliders = Physics2D.OverlapCircle(transform.position, 2.5f);
+            Collider2D[] colliders = Physics2D.OverlapCircleAll(transform.position, 2.5f);
             
             foreach (Collider2D collider in colliders) {
                 Task task;
